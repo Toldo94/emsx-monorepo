21dba1ec03ec6a109c4aa8deada1819d9b3c07c8
Nermin Hrustic <nermin.hrustic.ets@gmail.com>
2024-06-30 10:05:48 +0200

    fix headles ui

--- app/lib/ui/dark-section.tsx
+++ app/lib/ui/dark-section.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import clsx from 'clsx'
-import { ButtonProps } from "@headlessui/react";
 
 interface OwnProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     children: React.ReactNode
