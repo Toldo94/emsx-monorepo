1960d4c39251e1a320cf062dc3767067dbb3de90
Nermin Hrustic <30322604+Toldo94@users.noreply.github.com>
2024-08-05 09:32:05 +0000

    fix build

--- app/lib/routes/users.routes.ts
+++ app/lib/routes/users.routes.ts
@@ -1,6 +1,6 @@
 import { RouteCreator } from "./base.routes";
 
 export class UserRoutes {
-    static readonly register = RouteCreator.createRoute("/v1/users");
+    static readonly users = RouteCreator.createRoute("/v1/users");
     static readonly login = RouteCreator.createRoute("/auth/login");
 }
