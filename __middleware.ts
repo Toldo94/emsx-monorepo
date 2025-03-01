6407ad6aba750b762f3448ab1e93d34ef684088c
Nermin Hrustic <nermin.hrustic.ets@gmail.com>
2025-03-01 09:14:23 +0100

    return middleware

--- __middleware.ts
+++
@@ -1,7 +0,0 @@
-export { auth as middleware } from "@/auth"
-
-
-export const config = {
-    unstable_allowDynamicImport: true,
-    matcher: ["/((?!api|_next/static|_next/image|registration|favicon.ico).*)", "/api/admin/:path*"],
-}
