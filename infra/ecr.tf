2742a5e30a02e315241c29b3d174c353b0c9ebb0
Nermin Hrustic <30322604+Toldo94@users.noreply.github.com>
2025-03-03 11:18:57 +0000

    Add terraform config

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -6,3 +6,12 @@ resource "aws_ecr_repository" "emsx_monorepo" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_repository" "emsx_monorepo_2" {
+  name                 = "emsx-monorepo_2"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
